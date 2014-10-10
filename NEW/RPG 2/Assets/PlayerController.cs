d3a956c634df56561acdf297fd403d32ed5abc9f
valmanchev <vmanchev@yahoo.com>
2014-10-10 23:47:23 +0300

    added NEW version of the project with pickUp items

---
+++ NEW/RPG 2/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{	
+	public Slider energySlider;
+	public Slider timeSlider;
+	public Text moneyText;
+	private int defaultMoney;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUpEnergy")
+		{
+			other.gameObject.SetActive(false);
+			energySlider.value = 100;
+		}
+		else if (other.gameObject.tag == "PickUpTime") 
+		{
+			other.gameObject.SetActive(false);
+			timeSlider.value = 100;
+		}
+		else if (other.gameObject.tag == "PickUpMoney") 
+		{
+			other.gameObject.SetActive(false);
+			defaultMoney = int.Parse(moneyText.text);
+			defaultMoney = defaultMoney + 100;
+			moneyText.text = defaultMoney.ToString();
+		}
+	}
+
+}
