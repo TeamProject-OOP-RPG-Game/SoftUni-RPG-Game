1ebed6f8171db8756fbcdf0eeca57e6ef392c6c5
valmanchev <vmanchev@yahoo.com>
2014-10-15 14:27:47 +0300

    fixed bug

--- NEW/RPG 2/Assets/Scripts/GeneratePickUps.cs
+++ NEW/RPG 2/Assets/Scripts/GeneratePickUps.cs
@@ -7,18 +7,18 @@ public class GeneratePickUps : MonoBehaviour
 	public GameObject pickUpMoney;
 	public GameObject pickUpEnergy;
 
-	Vector3 theNewPos = new Vector3(Random.Range(0, 2000),1,Random.Range(0, 2000));
+	Vector3 theNewPos = new Vector3(Random.Range(0, 500),1,Random.Range(0, 500));
 
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < 20; i++) {
-			theNewPos = new Vector3(Random.Range(0, 2000),1,Random.Range(0, 2000));
+			theNewPos = new Vector3(Random.Range(0, 500),1,Random.Range(0, 500));
 			Instantiate(pickUpTime, theNewPos, transform.rotation);
 
-			theNewPos = new Vector3(Random.Range(0, 2000),1,Random.Range(0, 2000));
+			theNewPos = new Vector3(Random.Range(0, 500),1,Random.Range(0, 500));
 			Instantiate(pickUpMoney, theNewPos, transform.rotation);
 
-			theNewPos = new Vector3(Random.Range(0, 2000),1,Random.Range(0, 2000));
+			theNewPos = new Vector3(Random.Range(0, 500),1,Random.Range(0, 500));
 			Instantiate(pickUpEnergy, theNewPos, transform.rotation);
 		}
 	}
