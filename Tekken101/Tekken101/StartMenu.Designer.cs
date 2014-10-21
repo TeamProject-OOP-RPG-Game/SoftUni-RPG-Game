31f3cb7ee9f83eab1f432337e8e6253a9c3ee576
GeriCookie <gergana.bailova@gmail.com>
2014-10-21 23:57:39 +0300

    StartMenu

---
+++ Tekken101/Tekken101/StartMenu.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Tekken101
+{
+    partial class StartMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jin = new System.Windows.Forms.PictureBox();
+            this.christie = new System.Windows.Forms.PictureBox();
+            this.nina = new System.Windows.Forms.PictureBox();
+            this.lili = new System.Windows.Forms.PictureBox();
+            this.steve = new System.Windows.Forms.PictureBox();
+            this.baek = new System.Windows.Forms.PictureBox();
+            this.miguel = new System.Windows.Forms.PictureBox();
+            this.heihachi = new System.Windows.Forms.PictureBox();
+            this.leo = new System.Windows.Forms.PictureBox();
+            this.lee = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.jin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.christie)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nina)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lili)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.steve)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.miguel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heihachi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // jin
+            // 
+            this.jin.Image = global::Tekken101.Properties.Resources.jinRS;
+            this.jin.Location = new System.Drawing.Point(394, 68);
+            this.jin.Name = "jin";
+            this.jin.Size = new System.Drawing.Size(100, 174);
+            this.jin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.jin.TabIndex = 9;
+            this.jin.TabStop = false;
+            this.jin.MouseClick += StartGame;
+            // 
+            // christie
+            // 
+            this.christie.Image = global::Tekken101.Properties.Resources.christieS;
+            this.christie.Location = new System.Drawing.Point(182, 68);
+            this.christie.Name = "christie";
+            this.christie.Size = new System.Drawing.Size(100, 174);
+            this.christie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.christie.TabIndex = 8;
+            this.christie.TabStop = false;
+            this.Controls.Add(christie);
+            // 
+            // nina
+            // 
+            this.nina.Image = global::Tekken101.Properties.Resources.ninaRS;
+            this.nina.Location = new System.Drawing.Point(924, 68);
+            this.nina.Name = "nina";
+            this.nina.Size = new System.Drawing.Size(100, 174);
+            this.nina.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.nina.TabIndex = 7;
+            this.nina.TabStop = false;
+            this.Controls.Add(nina);
+            // 
+            // lili
+            // 
+            this.lili.Image = global::Tekken101.Properties.Resources.liliS;
+            this.lili.Location = new System.Drawing.Point(712, 68);
+            this.lili.Name = "lili";
+            this.lili.Size = new System.Drawing.Size(100, 174);
+            this.lili.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lili.TabIndex = 6;
+            this.lili.TabStop = false;
+            this.Controls.Add(lili);
+            // 
+            // steve
+            // 
+            this.steve.Image = global::Tekken101.Properties.Resources.steveS;
+            this.steve.Location = new System.Drawing.Point(1030, 68);
+            this.steve.Name = "steve";
+            this.steve.Size = new System.Drawing.Size(100, 174);
+            this.steve.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.steve.TabIndex = 5;
+            this.steve.TabStop = false;
+            this.Controls.Add(steve);
+            // 
+            // baek
+            // 
+            this.baek.Image = global::Tekken101.Properties.Resources.baekS;
+            this.baek.Location = new System.Drawing.Point(76, 68);
+            this.baek.Name = "baek";
+            this.baek.Size = new System.Drawing.Size(100, 174);
+            this.baek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baek.TabIndex = 4;
+            this.baek.TabStop = false;
+            this.Controls.Add(baek);
+            // 
+            // miguel
+            // 
+            this.miguel.Image = global::Tekken101.Properties.Resources.miguelS;
+            this.miguel.Location = new System.Drawing.Point(818, 68);
+            this.miguel.Name = "miguel";
+            this.miguel.Size = new System.Drawing.Size(100, 174);
+            this.miguel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.miguel.TabIndex = 3;
+            this.miguel.TabStop = false;
+            this.Controls.Add(miguel);
+            // 
+            // heihachi
+            // 
+            this.heihachi.Image = global::Tekken101.Properties.Resources.heihachiS;
+            this.heihachi.Location = new System.Drawing.Point(288, 68);
+            this.heihachi.Name = "heihachi";
+            this.heihachi.Size = new System.Drawing.Size(100, 174);
+            this.heihachi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.heihachi.TabIndex = 2;
+            this.heihachi.TabStop = false;
+            this.Controls.Add(heihachi);
+            // 
+            // leo
+            // 
+            this.leo.Image = global::Tekken101.Properties.Resources.leoRS;
+            this.leo.Location = new System.Drawing.Point(606, 68);
+            this.leo.Name = "leo";
+            this.leo.Size = new System.Drawing.Size(100, 174);
+            this.leo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.leo.TabIndex = 1;
+            this.leo.TabStop = false;
+            this.Controls.Add(leo);
+            // 
+            // lee
+            // 
+            this.lee.Image = global::Tekken101.Properties.Resources.leeS;
+            this.lee.Location = new System.Drawing.Point(500, 68);
+            this.lee.Name = "lee";
+            this.lee.Size = new System.Drawing.Size(100, 174);
+            this.lee.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lee.TabIndex = 0;
+            this.lee.TabStop = false;
+            this.Controls.Add(lee);
+            // 
+            // StartMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.jin);
+            this.Controls.Add(this.christie);
+            this.Controls.Add(this.nina);
+            this.Controls.Add(this.lili);
+            this.Controls.Add(this.steve);
+            this.Controls.Add(this.baek);
+            this.Controls.Add(this.miguel);
+            this.Controls.Add(this.heihachi);
+            this.Controls.Add(this.leo);
+            this.Controls.Add(this.lee);
+            this.Name = "StartMenu";
+            this.Text = "StartMenu";
+            ((System.ComponentModel.ISupportInitialize)(this.jin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.christie)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nina)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lili)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.steve)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.miguel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heihachi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lee)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+      
+        #endregion
+
+        private System.Windows.Forms.PictureBox lee;
+        private System.Windows.Forms.PictureBox leo;
+        private System.Windows.Forms.PictureBox miguel;
+        private System.Windows.Forms.PictureBox heihachi;
+        private System.Windows.Forms.PictureBox steve;
+        private System.Windows.Forms.PictureBox baek;
+        private System.Windows.Forms.PictureBox nina;
+        private System.Windows.Forms.PictureBox lili;
+        private System.Windows.Forms.PictureBox jin;
+        private System.Windows.Forms.PictureBox christie;
+    }
+}
---
+++ Tekken101/Tekken101/StartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tekken101
+{
+    public partial class StartMenu : Form
+    {
+        public StartMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            
+            Form theGame = new TheGame();
+            theGame.Controls.Add((PictureBox)sender);
+            theGame.Show();
+            this.Hide();
+        }
+
+    }
+}
